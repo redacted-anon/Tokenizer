f0a3f0ceeedc10f4daf865b0edf584f55e51c409
Tyler Schwartz <tylerschwartz98@gmail.com>
2026-02-27 16:55:23 -0500

    Initial commit

---
+++ tokenizer.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+void tokenize_input(char user_input[100])
+{
+    int index = 0;
+    char *token = strtok(user_input, " ");
+    
+    while (token != NULL)
+    {   
+        printf("Index: %d \t|\t Value: %s\n", index, token);
+        index ++;
+        token = strtok(NULL, " ");
+    }
+}
+
+
+void get_input(void)
+{
+    char s[100];
+
+    printf("\nEnter some text (type 'exit' to end the program): ");
+    fgets(s, sizeof(s), stdin);
+    s[strcspn(s, "\n")] = '\0';
+
+    if (strcmp(s, "exit") == 0)
+    {
+        exit(EXIT_SUCCESS);
+    } else
+    {
+        tokenize_input(s);
+    }
+}
+
+
+int main(void)
+{
+    get_input();
+    return 0;
+}
