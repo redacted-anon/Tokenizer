1eb96ab2c619a83e37dddd66d5784ff48009a40f
Tyler Schwartz <tylerschwartz98@gmail.com>
2026-02-27 17:46:03 -0500

    Added short versions of built in commands.

--- tokenizer.c
+++ tokenizer.c
@@ -13,16 +13,17 @@ void clear_screen(void)
     #endif    
 }
 
+
 void tokenize_input(char user_input[100])
 {
     int index = 0;
-    char *token = strtok(user_input, " ");
+    char *token = strtok(user_input, " ,");
     
     while (token != NULL)
     {   
         printf("\tIndex: %d \t|\t Value: %s\n", index, token);
         index ++;
-        token = strtok(NULL, " ");
+        token = strtok(NULL, " ,");
     }
 }
 
@@ -37,11 +38,11 @@ void get_input(void)
         fgets(s, sizeof(s), stdin);
         s[strcspn(s, "\n")] = '\0';
 
-        if (strcmp(s, "exit") == 0)
+        if (strcmp(s, "exit") == 0 || strcmp(s, "e") == 0) 
         {
             clear_screen();
             exit(EXIT_SUCCESS);
-        } else if (strcmp(s, "clear") == 0)
+        } else if (strcmp(s, "clear") == 0 || strcmp(s, "c") == 0)
         {
             clear_screen();
         } else
